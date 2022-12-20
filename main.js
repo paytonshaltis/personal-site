63077502d6b34bb5c21212ca9ea1838be066569a
Payton Shaltis <shaltisp@gmail.com>
2022-12-19 23:31:09 -0500

    Adds random background image.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const NUM_BG_IMAGES = 3;
+const BODY = document.querySelector("body");
+let currentBgImage = 0;
+
 // Disable tooltips on touch devices.
 if (!("ontouchstart" in document.documentElement)) {
   $(function () {
@@ -6,3 +10,20 @@ if (!("ontouchstart" in document.documentElement)) {
     });
   });
 }
+
+// Selects a random background image to display on the home page.
+const newBackground = () => {
+  // Keep trying until we get a number other than the current one.
+  let imageNumber = currentBgImage;
+  while (currentBgImage === imageNumber) {
+    imageNumber = Math.floor(Math.random() * NUM_BG_IMAGES) + 1;
+  }
+  BODY.style.background = `linear-gradient(rgba(0, 0, 0, 0.315), rgba(0, 0, 0, 0.315)),
+    url("./resources/background/bg-${imageNumber}-compressed.jpg")`;
+  BODY.style.backgroundSize = "cover";
+  BODY.style.backgroundPosition = "center";
+  BODY.style.backgroundRepeat = "no-repeat";
+  currentBgImage = imageNumber;
+};
+
+newBackground();
